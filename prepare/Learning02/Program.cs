c6e1cf96160e9e04ad1c32417fb78133aa98d532
Gary Godderidge <102984600+ggodderi@users.noreply.github.com>
2026-03-03 09:37:28 -0700

    Job Class working

--- prepare/Learning02/Program.cs
+++ prepare/Learning02/Program.cs
@@ -4,6 +4,20 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Learning02 World!");
+        Job myFirstJob = new Job();
+
+        myFirstJob._companyName = "Digitran";
+        myFirstJob._jobTitle = "Software Engineer";
+        myFirstJob._startYear = 1989;
+        myFirstJob._endYear = 1994;
+
+        myFirstJob.DisplayJob();
+
+        Job mySecondJob = new Job();
+        mySecondJob._companyName = "HP";
+        mySecondJob._jobTitle = "Software Engineer";
+        mySecondJob._startYear = 1994;
+        mySecondJob._endYear = 2022;
+        mySecondJob.DisplayJob();
     }
 }
