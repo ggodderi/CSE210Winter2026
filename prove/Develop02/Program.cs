84588fa35e802aac667d0858ef02a068f0a1e67d
Gary Godderidge <102984600+ggodderi@users.noreply.github.com>
2026-03-05 09:41:06 -0700

    Base menu working program class.

--- prove/Develop02/Program.cs
+++ prove/Develop02/Program.cs
@@ -1,11 +1,36 @@
 using System;
+using System.Diagnostics.CodeAnalysis;
+using System.Security.Permissions;
 
 class Program
 {
     static void Main(string[] args)
     {
         Menu menu= new Menu();
-
-        menu.ProcessMenu();
+        bool done = false;
+        int userResponse;
+        do 
+        {
+            userResponse = menu.ProcessMenu();
+            switch (userResponse)
+            {
+                case 1:
+                // Create new Journal Entry
+                // Add entry to list of journal entries
+                break;
+                case 2:
+                // Display Journal Entries
+                break;
+                case 3:
+                // Save journal to file
+                break;
+                case 4:
+                // Load journal from file
+                break;
+                case 5:
+                    done = true;
+                    break;
+            }
+        } while (!done);
     }
 }
