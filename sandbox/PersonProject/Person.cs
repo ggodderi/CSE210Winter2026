9effeabeb188e1c87c99b0928eb6c99494d21275
Gary Godderidge <102984600+ggodderi@users.noreply.github.com>
2026-03-17 09:40:18 -0600

    Added Person project and Test Project.

---
+++ sandbox/PersonProject/Person.cs
@@ -0,0 +1,57 @@
+
+class Person
+{
+    private string _firstName;
+    private string _lastName;
+    private int _age;
+    private int _weight;
+
+    public Person(string firstName, string lastName, int age, int weight)
+    {
+        _firstName = firstName;
+        _lastName = lastName;
+        SetAge(age);
+        SetWeight(weight);
+        // _age = age;
+        // _weight = weight;
+    }
+
+    public string GetPersonInformation()
+    {
+        return $"Name: {_firstName} {_lastName}, age: {_age}, weight: {_weight}";
+    }
+    
+    public int GetAge()
+    {
+        return _age;
+    }
+
+    public int GetWeight()
+    {
+        return _weight;
+    }
+
+    public void SetWeight(int weight)
+    {
+        if(weight < 4 || weight > 500)
+        {
+            Console.WriteLine("Invalid weight");
+        }
+        else
+        {
+            _weight = weight;
+        }
+    }
+
+    public void SetAge(int age)
+    {
+        if(age < 0 || age > 125)
+        {
+            Console.WriteLine("Invalid age");
+        }
+        else
+        {
+            _age = age;
+        }
+    }
+}
---
+++ sandbox/PersonProject/Program.cs
@@ -0,0 +1,54 @@
+﻿class Program
+{
+
+    public static void ProcessPerson(Person person)
+    {
+        if (person is Police police)
+        {
+            Console.WriteLine(police.GetPoliceInformation());
+        }
+        else if (person is Doctor doctor)
+        {
+            Console.WriteLine(doctor.GetDoctorInformation());
+        }
+        else
+        {
+            Console.WriteLine(person.GetPersonInformation());
+        }
+
+    }
+    public static void Main(string[] args)
+    {
+        Person person = new Person("Bob", "Ross", 55, 195);
+
+        Console.WriteLine(person.GetPersonInformation());
+
+        Doctor myDoctor = new Doctor("Ax", "Bob", "Budge", 75, 210);
+        Console.WriteLine(myDoctor.GetDoctorInformation());
+
+        
+        Doctor myDoctor2 = new Doctor("Knife", "Bobby", "Billy", 75, 210);
+        Console.WriteLine(myDoctor2.GetDoctorInformation());
+
+        Police myPolice = new Police("Gun", "Lance", "Bob", 43, 198);
+        Console.WriteLine(myPolice.GetPoliceInformation());
+
+        myPolice.SetWeight(myPolice.GetWeight() + 5);
+        myPolice.SetAge(myPolice.GetAge() + 1);
+        Console.WriteLine(myPolice.GetPoliceInformation());
+
+
+        List<Person> myPeople = new List<Person>();
+        myPeople.Add(person);
+        myPeople.Add(myDoctor);
+        myPeople.Add(myDoctor2);
+        myPeople.Add(myPolice);
+
+        foreach(Person tempPerson in myPeople)
+        {
+            // Console.WriteLine(tempPerson.GetPersonInformation());
+            ProcessPerson(tempPerson);
+        }
+
+    }
+}
