a0eba4fffaeb1bd9c2f40466d0dda26eb7614446
Gary Godderidge <102984600+ggodderi@users.noreply.github.com>
2026-03-31 10:53:32 -0600

    Added the OperatorOverloading project.  Plus initial code working.

---
+++ sandbox/OperatorOverloading/Program.cs
@@ -0,0 +1,27 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.Numerics;
+
+class Program
+{
+    public static void Main(string[] args)
+    {
+        Vector2D vector1 = new Vector2D(1,1);
+        Vector2D vector2 = new Vector2D(10,12);
+        Console.WriteLine(vector1.GetVectorDisplayString());
+        Console.WriteLine((vector1 + vector2).GetVectorDisplayString());
+        Console.WriteLine((vector1 - vector2).GetVectorDisplayString());
+
+        if (vector1 != vector2)
+        {
+            vector1++;
+            Console.WriteLine(vector1.GetVectorDisplayString());
+        }
+
+        if (vector2 > vector1)
+        {
+            vector1++;
+            Console.WriteLine(vector1.GetVectorDisplayString());
+        }
+
+    }
+}
---
+++ sandbox/OperatorOverloading/Vector2D.cs
@@ -0,0 +1,59 @@
+using System.Runtime.Intrinsics;
+
+class Vector2D
+{
+    private int X;
+    private int Y;
+
+    public Vector2D(int x, int y)
+    {
+        this.X = x;
+        this.Y = y;
+    }
+
+    private double VectorMagnitude => (X * X) + (Y * Y);
+
+    public static Vector2D operator +(Vector2D v1, Vector2D v2)
+    {
+        return new Vector2D(v1.X + v2.X, v1.Y + v2.Y);
+    }
+
+    public static Vector2D operator -(Vector2D v1, Vector2D v2)
+    {
+        return new Vector2D(v1.X - v2.X, v1.Y - v2.Y);
+    }
+
+    public static bool operator <(Vector2D v1, Vector2D v2)
+    {
+        return v1.VectorMagnitude < v2.VectorMagnitude;
+    }
+    
+    public static bool operator >(Vector2D v1, Vector2D v2)
+    {
+        return v1.VectorMagnitude >  v2.VectorMagnitude;
+    }
+
+    public static bool operator ==(Vector2D v1, Vector2D v2)
+    {
+        return v1.X == v2.X && v1.Y == v2.Y;
+    }
+
+    public static bool operator !=(Vector2D v1, Vector2D v2)
+    {
+        return v1.X != v2.X && v1.Y != v2.Y;
+    }
+
+    public static Vector2D operator ++(Vector2D v1)
+    {
+        v1.X += 1;
+        v1.Y += 1;
+        return v1;
+    }
+
+
+
+    public string GetVectorDisplayString()
+    {
+        return $"x: {X}, y: {Y}";
+    }
+}
